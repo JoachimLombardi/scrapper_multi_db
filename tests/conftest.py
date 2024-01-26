d034f7c257255426dcd642adf1c6c68f5a0adc65
Joachim Lombardi <lombardi.joachim@gmail.com>
2024-01-26 12:12:35 +0100

    test

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,7 +4,7 @@ from flask_.app import app
 
 # pytest conftest.py 
 
-@pytest.fixture() # fixture utilisée pour faire les tests
+@pytest.fixture()  # fixture utilisée pour faire les tests
 def app2():
     app.config.update({
         "TESTING": True,
@@ -22,7 +22,7 @@ def runner(app2):
     return app2.test_cli_runner()
 
 
-def test_api(client):                   # Chaque test commence par la fonction test_
+def test_api(client):  # Chaque test commence par la fonction test_
     response = client.get("/random")
     assert response.status_code == 200
 
@@ -31,20 +31,24 @@ def test_query(client):
     response = client.get("/search?query=love")
     assert response.status_code == 200
 
+
 def test_insert(mongo_collection):
     try:
         mongo_collection.insert_one({"quote": "test", "author": "test"})
     except Exception:
         pytest.fail("Could not insert into MongoDB")
 
+
 def test_verify_quotes(mongo_collection):
     for quote in mongo_collection.find({}):
         assert "quote" in quote
         assert "author" in quote
 
+
 def test_verify_count(mongo_collection):
     count = mongo_collection.count_documents({})
     assert count >= 0
+
 
 # testing the mongodb connection
 @pytest.fixture()
@@ -55,12 +59,15 @@ def mongo_connection():
     except Exception:
         pytest.fail("Could not yield DB client")
 
+
 @pytest.fixture()
 def mongo_collection(mongo_connection):
     return mongo_connection.scrapy_db.quotes
+
 
 def test_mongo_db(mongo_connection):
     try:
         mongo_connection.scrapy_db
     except Exception:
         pytest.fail("Could not connect to MongoDB")
+        
